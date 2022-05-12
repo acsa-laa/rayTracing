4444e7639f0bc2e3f0a7735e2063fe1bf7ac79e1
Acsa Arcanjo <44912056+acsa-laa@users.noreply.github.com>
2022-05-12 15:43:40 -0300

    parser funcionando

--- parser.h
+++ parser.h
@@ -31,12 +31,12 @@ public:
         RunningOptions ro;
         const char *scene_file_name = "scene_01.xml";
         FILE * file = fopen(scene_file_name, "rt");
-        if(file == NULL){
+        if(file == nullptr){
             std::cout << scene_file_name << std::endl;
         }
         tinyxml2::XMLDocument doc;
 
-        if ( doc.LoadFile( file ) != tinyxml2::XML_SUCCESS )
+        if ( doc.LoadFile( scene_file_name ) != tinyxml2::XML_SUCCESS )
             std::cout<< std::string{"The file "} + scene_file_name + std::string{" either is not available OR contains an invalid RT3 scene provided!"} << std::endl;
 
         auto RT3 = doc.RootElement();
